f3358c6514c3c44150818d041fee3c6a711d14cf
Beatific-angel <64799935+Beatific-Angel@users.noreply.github.com>
2022-07-03 11:05:06 +0300

    Create new solution
    Create new solution

---
+++ update_bot.py
@@ -0,0 +1,5 @@
+from contextlib import suppress
+from operator import index
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
