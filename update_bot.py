782db170ed7877ca95ea83ac7dd0ee803b7183a5
Beatific-angel <64799935+Beatific-Angel@users.noreply.github.com>
2022-07-05 11:06:16 +0300

    Update
    Update

--- update_bot.py
+++ update_bot.py
@@ -3,3 +3,9 @@ from operator import index
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from requests.auth import HTTPBasicAuth
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
+from twocaptcha import TwoCaptcha
